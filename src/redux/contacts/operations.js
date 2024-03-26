d5cb659111eaea24a9a2e5a9eb2a35f9821acbb7
Lilianademo <123247173+Lilianademo@users.noreply.github.com>
2024-03-26 19:29:01 +0200

    08-phonebook

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://651334de8e505cebc2e9af84.mockapi.io";
+axios.defaults.baseURL = "https://6602ca3f9d7276a755543ddd.mockapi.io/";
 
 export const getAllContacts = createAsyncThunk(
   "contacts/fetchAll",
